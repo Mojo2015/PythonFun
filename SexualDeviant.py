9ecea4959390ffa099f73f9e218bea78b9395fc3
MojoDON <Lemongelloj@gmail.com>
2014-11-29 18:22:12 -0500

    Add SexualDeviant.py

---
+++ SexualDeviant.py
@@ -0,0 +1,116 @@
+##Determining if you are a phaggobond##
+"""
+You mirin' ??
+Created by Jomomohomo
+11/69/2014
+"""
+    
+Spam = 1
+Anus = 2
+
+print("1. Penis")
+print("2. Vagina")
+print("3. Transvestite Smegma")
+print("4. Transvestite with Vagina")
+print("5. Hermaphrodidic Dojo Ninja")
+print("\r\n")
+PrimeSelect = int(input("Select a number of your favorite form of sexual stimubations (enter an integer): "))
+
+if (PrimeSelect == 1):
+    print("Ok so you're a phaggot, select one of the following: ")
+    print("1. Pitching")
+    print("2. Receiving")
+
+    FaggySelect = int(input("Enter either 1 or 2: "))
+
+    if (FaggySelect == 1):
+        print("You're a phaggot, but atleast you aren't taking it" +
+              " in the anoose")
+    elif (FaggySelect == 2):
+        print("You're a full on phaggot. You like dicks and you like them in your ass" +
+              " I feel bad for your parents phaggot") 
+    else:
+        print("Come on, we already know you're gay, you don't have to lie about the second part")
+
+elif (PrimeSelect == 2):
+    print("Select an option")
+    print("1. Wearing a condom")
+    print("2. Not wearing a condom")
+
+    StraightSelect = int(input("Select either 1 or 2 buddy: "))
+
+    if (StraightSelect == 1):
+        print("Good on you bro, you literally just beat the game")
+        print("But you're still a phaggot")
+
+    elif (StraightSelect == 2):
+        print("---------> Babynames.com for you daddy boyo")
+
+    else:
+        print("I don't believe you're straight if you can get this far then back out")
+
+elif (PrimeSelect == 3):
+    print("I really don't have any other questions for you, you disgusting piece of trash")
+    print("You have but two options left....")
+    print("1. Kill yourself")
+    print("2. Kill yourself")
+
+    DisgustingSelect = int(input("Select either 1 or 2 you disgusting fuck...: "))
+    
+
+
+    while (DisgustingSelect == 1 or DisgustingSelect == 2):
+        Spam += 1
+        print("KILL YOURSELF YOU DISGUSTING TURD MOTHER FUCKER", Spam)
+
+    else:
+        Lol = input("Did you think you could get off the hook that easily: ")
+
+        if (Lol == 239234792349237):
+            print("Hack your fucking face off")
+        else:
+            while (Lol != 239234792349237):
+                Spam += 52
+                Anus += 69
+                print ("YOU'RE STILL GOING TO FUCKING HELL YOU PIECE OF SHIT ANUS BAG", Spam, Anus, Spam * Anus)
+
+elif (PrimeSelect == 4):
+    print("Well, do you think it's gay to fuck a dude who is now a vagina bag chick?")
+
+    Hmm = str.lower(input("Enter Yes or No: "))
+
+    while (Hmm == "yes"):
+          Spam += 549
+          Anus *= 2
+          print ("Wow, you're a smelly fucking potato bro!", Spam, Anus, Spam / Anus, Spam * Anus)
+
+    else:
+          print("Well I disagree, but to each their own... phaggot")
+
+elif (PrimeSelect == 5):
+    print("I'm 12 and what is this?")
+    Ninja = str.lower(input("Do you like to suck ninja cock? Enter an answer: "))
+    Ninja = 1
+    if (Ninja == 1):
+            print("Fuck off and die you stupid cocksucker")
+
+
+else:
+    print ("Wow, you are a lame cocksucker.. here suck on this 8====D~~~~~~")
+    print ("phaggot")
+
+    import time
+    time.sleep(3)
+    print ("Did you have a nice nap?")
+
+    time.sleep(2)
+    print("Watch this phaggot")
+
+    time.sleep(1)
+    Phaggot = 1
+    while (Phaggot == 1):
+           print ("You have a serious addiction to mammoth cocks in your asshole you gay son of a BITCH")
+
+           
+           
+
