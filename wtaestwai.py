40db250e30fcb0bd88ee1cd33f043501a7657a25
suprememomo <mohaskedium@gmail.com>
2014-11-30 03:39:54 -0500

    Johamed Fun Collab
    We are AWESOME

---
+++ wtaestwai.py
@@ -0,0 +1,64 @@
+"""
+Lets program GO
+Oparation MojojoMy
+brAINO
+Overnight Vagina sensations
+8=====D~~~~
+"""
+
+import sys
+import time
+
+def SmegmaMan():
+    print("Do you want to play again you sexy son of a BITCH")
+    Replay = str.lower(input("Yes or no sexy pants: "))
+    if (Replay == "yes"):
+        from Primary import Primary
+        Primary()
+    else:
+        print("FYinoeu  bro, suck yourself phaggot")
+        time.sleep(2)
+        quit()
+        
+
+        
+    
+def WorkMan():
+    
+    print("I wish mohamed would add some saweeeet lines of code")
+    print("fuck it GO~!")
+    time.sleep(2)
+
+    Semen = 0
+    NumCount = 0
+    while (Semen <= 69):
+        print ("You have a small penis", NumCount)
+        Semen += 1
+        NumCount += 1
+        print ("\r\n")
+
+    if (Semen >= 69):
+        SmegmaMan()
+
+
+    WorkMan()
+
+
+def Polygon():
+    print("How many sides does a Polygon have")
+    Replay = str.lower(input("Is this a trick question: "))
+    if (Replay == "yes"):
+        print("Well look at that if it isnt the next Einstein", "\r\n")
+        time.sleep(2)
+        SmegmaMan()
+    else:
+        print("you are one dumb fuck!")
+        time.sleep(2)
+        print("\n")
+        SmegmaMan()
+
+    
+
+
+
+           
