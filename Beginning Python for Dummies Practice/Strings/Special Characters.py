c131967307eda183b589ba7b74d6cba8abd15171
MojoDON <Lemongelloj@gmail.com>
2014-12-02 01:46:44 -0500

    update2

--- Beginning Python for Dummies Practice/Strings/Special Characters.py
+++ Beginning Python for Dummies Practice/Strings/Special Characters.py
@@ -1,8 +1,8 @@
-print("Part of this text\r\nis on the next line.")
-print("This is an A with a grave accent: \xC0.")
-print("This is a drawing character: \u2562.")
-print("This is a pilcrow: \266.")
-print("This is a division sign: \xF7.")
+##print("Part of this text\r\nis on the next line.")
+##print("This is an A with a grave accent: \xC0.")
+##print("This is a drawing character: \u2562.")
+##print("This is a pilcrow: \266.")
+##print("This is a division sign: \xF7.")
 
 MyString = "Hello World"
 print(MyString[0:12])
@@ -10,12 +10,12 @@ print(MyString[0:12])
 String1 = "Hello World"
 String2 = "Python is Fun!"
 
-print(String1[0])
-print(String1[0:5])
-print(String1[:5])
-print(String1[6:])
+##print(String1[0])
+print(String1[-3:])
+##print(String1[:5])
+##print(String1[6:])
 
-String3 = String1[:6] + String2 [:6]
-print(String3)
-
-print(String2[:7]*5)
+##String3 = String1[:6] + String2 [:6]
+##print(String3)
+##
+##print(String2[:7]*5)
--- Tkinter/gif download and display.py
+++ Tkinter/gif download and display.py
@@ -1,30 +1,42 @@
 import random
 import urllib.request
 import sys
+import os
 from tkinter import *
+import webbrowser
+
 
     
 def download_web_image(url):
     global name
+    global full_name
     name = str(input("what do you want to name it? "))
-    full_name = str(name) + ".gif" #str(name) will convert it to a string
+    full_name = str(name) + "." + str(Define_URL[-3:]) #str(name) will convert it to a string
     urllib.request.urlretrieve(url, full_name)
+    print(Define_URL[-3:])
     return name
 
+global Define_URL
 Define_URL = str(input("Paste the URL here: "))
 download_web_image(Define_URL)
 
-def my_gui():
-    my_gui = Tk()
-##    my_gui.geometry('800x600+450+300')
+os.startfile(full_name)
+import webbrowser
+webbrowser.open(full_name)
 
-    photo = PhotoImage(file=str(name) + '.gif')
-    label = Label(my_gui, image=photo)
-    label.pack()
 
-    my_gui.mainloop()
 
-my_gui()
+##def my_gui():
+##    my_gui = Tk()
+####    my_gui.geometry('800x600+450+300')
+##
+##    photo = PhotoImage(file=str(full_name)) #+ '.gif')
+##    label = Label(my_gui, image=photo)
+##    label.pack()
+##
+##    my_gui.mainloop()
+##
+##my_gui()
 
 
 
