d28496b9ccbae4569f0dacce8b021bb85fb88ced
mojo2015 <lemongelloj@gmail.com>
2014-12-03 00:28:11 -0500

    second ubuntu push

---
+++ Ubuntu/First.py
@@ -0,0 +1,5 @@
+print("this is a test file")
+print("I can barely remember anything and must get back to work")
+print("The only way to master this is to work every day")
+
+print("Have fune working via linux")
